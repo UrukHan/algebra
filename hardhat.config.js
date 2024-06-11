633dc4e90abeb1e80ef79ab20f53001f38d30345
a.urumov <urumov@easydev.group>
2024-06-11 16:46:33 +0300

    0.0.2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: {
@@ -11,7 +12,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://mainnet.infura.io/v3/534490e9efe74119b3cc51a400f20076",
+        url: `https://mainnet.infura.io/v3/${process.env.API_KEY}`,
       },
     },
   },
